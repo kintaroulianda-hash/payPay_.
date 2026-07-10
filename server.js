7e8da574f9f62af36f95f808a6bfee76f355e55c
Linux.love <260929695+z625137-maker@users.noreply.github.com>
2026-07-11 00:41:09 +0900

    Update Access log import path in server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const { getAccessLog } = require("./dist/Access");
+const { getAccessLog } = require("./Access.tsx");
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("."));
